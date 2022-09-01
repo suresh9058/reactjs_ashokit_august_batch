69d8b30a4726ce41a3adf14911a8e8f4a9a5c4bb
suresh9058 <suresh9058@gmail.com>
2022-09-01 16:39:07 +0530

    Reactjs basic component creation.

---
+++ src/Welcome.js
@@ -0,0 +1,10 @@
+function Welcome() {
+    return (
+        <>
+            <h2>Welcome Component {Math.floor(Math.random()*30)}</h2>
+            <p>jsx problem</p>
+        </>    
+    )
+}
+
+export default Welcome;
